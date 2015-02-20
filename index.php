8a34e3ac80630cd1429add351c2f820c839dfa0f
Mewt <agungsukariman@gmail.com>
2015-02-21 00:32:48 +0700

    test commit with tortoise

--- index.php
+++ index.php
@@ -2,7 +2,18 @@
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-	<title></title>
+	<script type="text/javascript">
+	function delete_user(id){
+		
+		var jawaban = confirm('Apa Anda yakin?');
+		if (jawaban){
+			//direct ke delete.php
+			window.location='delete.php?id='+id;
+		}
+	}
+	
+	</script>
+	<title>Index ~ Data Tersimpan</title>
 </head>
 <body>
 	<!--dinamik kontent-->
@@ -10,7 +21,54 @@
 	<?php
 	//koneksi database
 	include 'config/db_connect.php';
+	$action = isset($_GET['action'])?$_GET['action']:"";
+	//kondisi cek
+	if ($action=='deleted') {
+	  	echo "<div>Data telah dihapus</div>";
+	}
 	
+	//pilih semua data
+	$query ="SELECT id, firstname,lastname,username FROM users";
+	$stmt =$con->prepare($query);
+	$stmt->execute();
+	
+	//
+	$num = $stmt->rowCount();
+	//
+	if($num>0){
+		//table
+		echo "<table border='1'>";
+		
+		//membuat table
+		echo "<tr>";
+			echo "<th>Firstname</th>";
+			echo "<th>Lastname</th>";
+			echo "<th>Username</th>";
+			echo "<th>Action</th>";
+		echo "<tr/>";
+		//menerima data dari database dengan fungsi fetch
+		while ($row =$stmt->fetch(PDO::FETCH_ASSOC)) {
+		     extract($row);
+			 //membuat table baru
+			 echo "<tr>";
+			 	echo "<td>{$firstname}</td>";
+				echo "<td>{$lastname}</td>";
+				echo "<td>{$username}</td>";
+				echo "<td>";
+				echo "<a href='edit.php?=id={$id}'>EDIT</a>";
+				echo "/";			
+				echo "<a href='#' onclick='delete_user({$id});'>Delete</a>";
+				echo "</td>";
+			echo "</tr>";
+				
+		}
+	}
+		//akhir table
+		else {
+		       echo "Tidak ada data";
+		
+		}
+		
 	?>
 </body>
 </html>
